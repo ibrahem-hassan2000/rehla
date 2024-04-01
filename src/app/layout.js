9a3a70405fadaf31727870a9c8386378a941e1fd
ibrahem hassan <hibrahem266@gmail.com>
2024-04-01 14:26:11 +0200

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+import LayNav from "../../Components/LayNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LayNav/>
+        {children}
+        <Script src="/bootstrap.js" />
+      
+      </body>
+
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,325 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <section className="investor">
+        <div className="container">
+          <div className="parts">
+            <div className="part">
+              <img src="/images/img1.webp" alt="" />
+            </div>
+            <div className="part">
+              <img src="/images/img2.webp" alt="" />
+            </div>
+            <div className="part">
+              <img src="/images/img3.webp" alt="" />
+            </div>
+            <div className="part">
+              <img src="/images/img4.webp" alt="" />
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
+      </section>
+      <section className="services">
+        <div className="container">
+          <h2 className="headTitle">
+            <span>Rehla</span> Services{" "}
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="dec">
+            Fully layered dolor sit amet, nobis id expedita dolores officiis
+            laboriosam.
           </p>
-        </a>
-      </div>
+          <div className="parts">
+            <div className="part">
+              <img src="/images/serv1.webp" alt="service 1" />
+              <h3>I'm going to</h3>
+              <p>
+                Passengers order a ride with a local driver to go to another
+                place inside the city Ride-hailing.
+              </p>
+            </div>
+            <div className="part">
+              <img src="/images/serv1.webp" alt="service 1" />
+              <h3>I'm going to</h3>
+              <p>
+                Passengers order a ride with a local driver to go to another
+                place inside the city Ride-hailing.
+              </p>
+            </div>
+            <div className="part">
+              <img src="/images/serv1.webp" alt="service 1" />
+              <h3>I'm going to</h3>
+              <p>
+                Passengers order a ride with a local driver to go to another
+                place inside the city Ride-hailing.
+              </p>
+            </div>
+            <div className="part">
+              <img src="/images/serv1.webp" alt="service 1" />
+              <h3>I'm going to</h3>
+              <p>
+                Passengers order a ride with a local driver to go to another
+                place inside the city Ride-hailing.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="news">
+        <div className="container">
+          <div className="parts">
+            <div className="part1">
+              <img src="/images/iphoneNews.webp" alt="iphoneNews" />
+            </div>
+            <div className="part2">
+              <h2 className="headTitle">
+                Stay tuned for new newsThey are coming with offers{" "}
+              </h2>
+              <h3>
+                An enim nullam tempor gravida donec enim luctus risusd diam eget
+                risus varius blandit sit amet.
+              </h3>
+              <ul>
+                <li>
+                  <h4>Feature Integration</h4>
+                  <p>
+                    We believe that designing products and services in close is
+                    the only way to have a real impact on their business.
+                  </p>
+                </li>
+                <li>
+                  <h4>Multiple Discussions</h4>
+                  <p>
+                    We believe that designing products and have aclients is the
+                    only way to have a real impact on their business.
+                  </p>
+                </li>
+              </ul>
+              <a href="#" className="btnPage">
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="steps">
+        <div className="container">
+          <div className="parts">
+            <div className="part1">
+              <h2 className="headTitle">
+                Join tourism experts as a <span>Rehla</span> traveler
+              </h2>
+              <div className="phoneImages">
+                <img src="/images/phoneImg1.webp" alt="phoneImage1" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage2" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage3" />
+              </div>
+            </div>
+            <div className="part2">
+              <div className="step">
+                <div className="num">
+                  <span>1</span>
+                </div>
+                <div className="info">
+                  <h3>First Step</h3>
+                  <p>
+                    Please Click on the Join Now and fill in your data as a
+                    tourist traveler
+                  </p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>2</span>
+                </div>
+                <div className="info">
+                  <h3>Second Step</h3>
+                  <p>Fill out your tourism information form</p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>3</span>
+                </div>
+                <div className="info">
+                  <h3>Third Step</h3>
+                  <p>
+                    After reviewing our data, you will be able to create tours
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="steps rev">
+        <div className="container">
+          <div className="parts">
+            <div className="part1">
+              <h2 className="headTitle">
+                Book your prebooking <span>Rehla</span> trip now
+              </h2>
+              <div className="phoneImages">
+                <img src="/images/phoneImg1.webp" alt="phoneImage1" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage2" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage3" />
+              </div>
+            </div>
+            <div className="part2">
+              <div className="step">
+                <div className="num">
+                  <span>1</span>
+                </div>
+                <div className="info">
+                  <h3>First Step</h3>
+                  <p>
+                    Login with your account for Rehla or create an account for
+                    the first time through our website or App
+                  </p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>2</span>
+                </div>
+                <div className="info">
+                  <h3>Second Step</h3>
+                  <p>Go to Prebooking Trips</p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>3</span>
+                </div>
+                <div className="info">
+                  <h3>Third Step</h3>
+                  <p>
+                    Select your destination and departure date and book now and
+                    wait for your offer to be accepted by one of our captains
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="steps">
+        <div className="container">
+          <div className="parts">
+            <div className="part1">
+              <h2 className="headTitle">
+                Join tourism experts as a <span>Rehla</span> traveler
+              </h2>
+              <div className="phoneImages">
+                <img src="/images/phoneImg1.webp" alt="phoneImage1" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage2" />
+                <img src="/images/phoneImg1.webp" alt="phoneImage3" />
+              </div>
+            </div>
+            <div className="part2">
+              <div className="step">
+                <div className="num">
+                  <span>1</span>
+                </div>
+                <div className="info">
+                  <h3>First Step</h3>
+                  <p>
+                    Please Click on the Join Now and fill in your data as a
+                    tourist traveler
+                  </p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>2</span>
+                </div>
+                <div className="info">
+                  <h3>Second Step</h3>
+                  <p>Fill out your tourism information form</p>
+                </div>
+              </div>
+              <div className="step">
+                <div className="num">
+                  <span>3</span>
+                </div>
+                <div className="info">
+                  <h3>Third Step</h3>
+                  <p>
+                    After reviewing our data, you will be able to create tours
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="sectionCards">
+        <div className="container">
+          <span className="headCards">Featured coures</span>
+          <h2 className="headTitle">
+            Charge <span>Rehla</span> Cards Now
+          </h2>
+          <p className="dec">
+            An enim nullam tempor sapien gravida donec enim ipsum porta justo
+            integer at odio velna vitae auctor integer.
+          </p>
+          <div className="cards">
+            <div className="card">
+              <div className="imgBox">
+                <img alt="card name img" src="/images/card1.jpeg"/>
+              </div>
+              <div className="cardInfo">
+                <div className="headPrice">
+                  <h3>
+                    <span>Rehla - </span> 15 SAR
+                  </h3>
+                </div>
+                <div className="price">
+                  <h4>
+                    <span>Price: </span> 15 SAR
+                  </h4>
+                </div>
+                <a href="" className="btnPage">Charge wallet</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="imgBox">
+                <img alt="card name img" src="/images/card1.jpeg"/>
+              </div>
+              <div className="cardInfo">
+                <div className="headPrice">
+                  <h3>
+                    <span>Rehla - </span> 15 SAR
+                  </h3>
+                </div>
+                <div className="price">
+                  <h4>
+                    <span>Price: </span> 15 SAR
+                  </h4>
+                </div>
+                <a href="" className="btnPage">Charge wallet</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="imgBox">
+                <img alt="card name img" src="/images/card1.jpeg"/>
+              </div>
+              <div className="cardInfo">
+                <div className="headPrice">
+                  <h3>
+                    <span>Rehla - </span> 15 SAR
+                  </h3>
+                </div>
+                <div className="price">
+                  <h4>
+                    <span>Price: </span> 15 SAR
+                  </h4>
+                </div>
+                <a href="" className="btnPage">Charge wallet</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
